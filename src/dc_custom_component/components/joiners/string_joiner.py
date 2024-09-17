a74c2841dc78ed69cf39ea90c002c8dd706d06da
Sebastian Husch Lee <sjrl423@gmail.com>
2024-09-17 14:05:08 +0200

    Add string joiner

---
+++ src/dc_custom_component/components/joiners/string_joiner.py
@@ -0,0 +1,23 @@
+from typing import Dict, List
+
+from haystack import component, logging
+from haystack.core.component.types import Variadic
+
+logger = logging.getLogger(__name__)
+
+
+@component
+class StringJoiner:
+    """
+    Component to join strings from different components to a list of strings
+    """
+
+    @component.output_types(strings=List[str])
+    def run(self, strings: Variadic[str]) -> Dict[str, List[str]]:
+        """
+        Joins strings into a list of strings
+        :param strings: strings from different components
+
+        """
+        strings = list(strings)
+        return {"strings": strings}
